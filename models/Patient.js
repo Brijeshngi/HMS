127ee18b453b334af7266c63d456c1a906493ec9
Brijesh Gupta <brijeshngi@gmail.com>
2023-05-14 15:12:24 +0530

    modified

--- models/Patient.js
+++ models/Patient.js
@@ -4,46 +4,57 @@ import validator from "validator";
 const schema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    maxlength: [25, "don't exceed 25 characters"],
+    required: [true, "Name required"],
   },
   gender: {
     type: String,
     enum: ["Male", "Female"],
+    required: [true, "Gender Required"],
   },
   age: {
     type: String,
-    required: true,
+    required: [true, "Age required"],
   },
   weight: {
     type: String,
-    required: true,
+    required: [true, "Weight required"],
   },
   contact: {
     type: String,
-    required: true,
+    minlength: [10, "minimum 10 digit required"],
+    maxlength: [10, "maximum 10 digit required"],
+    required: [true, "Contact required"],
   },
   email: {
     type: String,
+    required: l[(true, "Email required")],
     validate: validator.isEmail,
   },
   address: {
     type: String,
-    required: true,
+    minlength: [20, "minimum 20 digit required"],
+    maxlength: [50, "maximum 50 digit required"],
+    required: [true, "address reuired"],
   },
-  department: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Department",
-  },
-  doctor: [
+  department: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Doctor",
+      name: { type: mongoose.Schema.Types.ObjectId, ref: "Department" },
+    },
+    {
+      doctor: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
     },
   ],
+
   lab_reports: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Lab_report",
+    },
+  ],
+  prescription: [
+    {
+      type: Array,
     },
   ],
   bill: [
@@ -54,13 +65,25 @@ const schema = mongoose.Schema({
   ],
   status: {
     type: String,
+    required: [true, "status required"],
     enum: ["IPD", "OPD"],
+  },
+  date_of_admission: {
+    type: Date,
+    Date: Date.now(),
   },
   patient_status: {
     type: String,
     enum: ["Admit", "Discharge"],
   },
+  date_of_discharge: {
+    type: Date,
+    Date: Date.now(),
+  },
   summary: {
+    type: String,
+  },
+  advice: {
     type: String,
   },
   createdAt: {
